197bc227765c0cc8566fcfee41d49cd4ab2f617c
Nabil Sabi <nabilsabi01@gmail.com>
2024-06-28 00:36:01 +0100

    create structure the project spring boot

---
+++ diabetes-tracker-backend/src/main/java/com/enaa/diabetes/DiabetesTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enaa.diabetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ diabetes-tracker-backend/src/test/java/com/enaa/diabetes/DiabetesTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.diabetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
